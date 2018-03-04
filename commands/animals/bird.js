408c97491c928fe79527e453267f20c626aca3e4
YoussefElshemi <33766341+YoussefElshemi@users.noreply.github.com>
2018-03-04 03:37:25 +0000

    Create bird.js

---
+++ commands/animals/bird.js
@@ -0,0 +1,40 @@
+const Social = require(`${process.cwd()}/base/Social.js`);
+const snekfetch = require("snekfetch");
+class Bird extends Social {
+  constructor(client) {
+    super(client, {
+      name: "bird",
+      description: "Post a randomly selected image of a bird.",
+      category: "Animals",
+      usage: "bird",
+      extended: "This command will return a beautiful bird.",
+      cost: 5,
+      cooldown: 10,
+      aliases: ["birb"]
+    });
+  }
+
+  async run(message, args, level) { // eslint-disable-line no-unused-vars
+    try {
+      if (message.settings.socialSystem === "true") {
+        if (!(await this.cmdPay(message, message.author.id, this.help.cost))) return;
+      }
+      const msg = await message.channel.send(`<a:typing:397490442469376001> **${message.member.displayName}** is petting a bird...`);
+      const { body } = await snekfetch.get("http://random.birb.pw/tweet/");
+      await msg.edit({
+        embed: {
+          "title": "Click here if the image failed to load.",
+          "url": `https://random.birb.pw/img/${body}`,
+          "color": message.guild.me.roles.highest.color || 5198940,
+          "image": {
+            "url": `https://random.birb.pw/img/${body}`
+          }
+        }
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+}
+
+module.exports = Bird;
